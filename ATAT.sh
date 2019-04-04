ab3b5e30c7d0aa3c5bbf9986514c121b705df105
Unknown <e777a63f@opayq.com>
2019-04-03 23:59:38 -0500

    bugfix

--- ATAT.sh
+++ ATAT.sh
@@ -1513,6 +1513,17 @@ do
 				sleep 15
 				~/ATAT/ATAT.sh
 				else
+		echo -e "\e[1;34m[*] Performing an APT Update prior to installing dependencies...\e[0m\n"
+		sleep 3
+		apt-get update
+		echo ""
+		echo -e "\e[1;32m[+] APT Update complete...\e[0m"
+		sleep 3
+		echo -e "\e[1;34m[*] Please wait while I install some dependencies...\e[0m\n"
+		sleep 3
+#		updatedb
+		mkdir /tmp/ATAT/
+		echo ""
 reqs="gcc gcc-mingw-w64-i686 curl jq bettercap macchanger metasploit-framework john nmap armitage apache2 lbd masscan msfpc sslscan libssl-dev libnl-genl-3-dev hostapd-wpe lynx airgeddon hostapd lighttpd asleap python-pip python-scapy gawk libatk-adaptor libgail-common bloodhound libxml2-dev libxslt1-dev unixodbc-dev git libssl1.0-dev libffi-dev python-dev tcpdump python-virtualenv p7zip"
 	for i in $reqs; do
 		dpkg -s "$i" &> /tmp/ATAT/$i-install.txt
